697272daf00636c91cd5909620b34072ee4dd7c8
DaniEspinoza0508 <gedaeszu06@gmail.com>
2025-05-28 20:53:31 -0600

    Add files via upload

---
+++ Caso Evaluado 1/src/caso/evaluado/pkg1/CasoEvaluado1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package caso.evaluado.pkg1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Genesis
+ */
+public class CasoEvaluado1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Solicite el salario a la enfermera para pagar deuda de la CCSS
+       lectura = JOptionPane.showInputDialog("Ingrese su salario");          
+    }
+    
+}
+
